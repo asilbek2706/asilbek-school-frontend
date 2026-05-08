83435ab1dbe7dd168492ec36cd809ad90b90dc79
Asilbek <asilbekkaromatov2@gmail.com>
2026-05-08 12:23:19 +0500

    heelo

--- components/Input.tsx
+++ components/Input.tsx
@@ -9,7 +9,6 @@ const SearchInput = () => {
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
           </div>
-
           <input
             type="text"
             placeholder="Kurslarni qidirish..."
